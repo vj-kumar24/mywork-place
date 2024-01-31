66ece5cc8cb45759f2ff0930be54a2362ecdcbd6
Vjkumar24 <m.d.vijaykumar24@gmail.com>
2024-01-31 15:10:34 +0530

    jenkins script for amazon linux3

---
+++ jenkins.sh
@@ -0,0 +1,6 @@
+yum update -y
+yum install java-17 -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+yum install jenkins -y
+systemctl start jenkins
